dc0ab0569d5b8a3ae9dbef41c2fe726b8ead4125
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-04-10 18:51:09 +0300

    set up relationships

--- server/models.py
+++ server/models.py
@@ -2,7 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
 
-metadata = MetaData()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
+})
 
 db = SQLAlchemy(metadata=metadata)
 
@@ -19,6 +21,10 @@ class Exercise(db.Model):
     category = db.Column(db.String)
     equipment_needed = db.Column(db.Boolean)
     
+    workout_exercise = db.relationship('WorkoutExercise', back_populates = 'exercise')
+    workouts = db.relationship('Workout', back_populates = 'exercises', secondary = 'workout_exercises')
+    
+    
     def __repr__(self):
         return f'<Exercise {self.id}, {self.name}, {self.category}, {self.equipment_needed}>'
     
@@ -30,10 +36,13 @@ class Workout(db.Model):
     duration_minutes = db.Column(db.Integer)
     notes = db.Column(db.Text)
     
+    workout_exercise = db.relationship('WorkoutExercise', back_populates = 'workout')
+    exercises = db.relationship('Exercise', back_populates = 'workouts', secondary='workout_exercises')
+    
     def __repr__(self):
         return f'<Workout {self.id}, {self.date}, {self.duration_minutes}, {self.notes}>'
     
-class WorkoutExercises(db.Model):
+class WorkoutExercise(db.Model):
     __tablename__ = 'workout_exercises'
     
     id = db.Column(db.Integer, primary_key = True)
@@ -43,3 +52,10 @@ class WorkoutExercises(db.Model):
     
     workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'))
     exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'))
+    
+    workout = db.relationship('Workout', back_populates = 'workout_exercise')
+    exercise = db.relationship('Exercise', back_populates = 'workout_exercise')
+    
+    def __repr__(self):
+        return f'<WorkoutExercise {self.id}, {self.reps}, {self.sets}, {self.duration_seconds}>'
+    
