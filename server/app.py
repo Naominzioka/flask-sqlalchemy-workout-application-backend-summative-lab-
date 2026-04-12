b54e857f2d17bed7669532d7fd52aa627f420343
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-04-12 20:11:39 +0300

    setup endpoints for workouts

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
+from schema import *
 
 from models import *
 
@@ -20,22 +21,58 @@ def home():
 #list workouts
 @app.route('/workouts', methods=['GET'])
 def get_workouts():
-    return make_response({"message": "List of all workouts"}, 200)
+    workouts_list = Workout.query.all()
+    if not workouts_list:
+        return make_response({"message": "No workouts found"}, 404)
+    response_body = WorkoutSchema(many=True).dump(workouts_list)
+    return make_response(response_body, 200)
 
 #get workout by id
 @app.route('/workouts/<int:id>', methods=["GET"])
 def get_workout_id(id):
-    return make_response({"message": f"Show workout {id} with exercises"}, 200)
+    workout = Workout.query.filter(Workout.id == id).first()
+    if not workout:
+        return make_response({"message": "No workout with that id found"}, 404)
+    response_body = WorkoutSchema().dump(workout)
+    return make_response(response_body, 200)
 
 #add a new workout
 @app.route('/workouts', methods=['POST'])
 def create_workout():
-    return make_response({"message": "Create a workout"}, 201)
+    if not request.is_json:
+        return make_response({"message": "Request body must be JSON"}, 400)
+
+    data = request.get_json()
+
+    try:
+        validated_data = WorkoutSchema().load(data)
+    except ValidationError as e:
+        return make_response({"errors": e.messages}, 400)
+
+    try:
+        new_workout = Workout(
+            date=validated_data.get('date'),
+            duration_minutes=validated_data.get('duration_minutes'),
+            notes=validated_data.get('notes')
+        )
+        db.session.add(new_workout)
+        db.session.commit()
+    except ValueError as e:
+        return make_response({"message": str(e)}, 400)
+
+    response_body = WorkoutSchema().dump(new_workout)
+    return make_response(response_body, 201)
 
 #delete a workout
 @app.route('/workouts/<int:id>', methods=['DELETE'])
 def delete_a_workout(id):
-    return make_response({"message": f"Workout with {id} was successfully deleted"}, 204)
+    workout_to_delete = Workout.query.filter(Workout.id == id).first()
+    if not workout_to_delete:
+        return make_response({"message": "Workout id not found"}, 404)
+    db.session.delete(workout_to_delete)
+    db.session.commit()
+    return make_response('', 204)
+    
 
 #exercises
 #get all exercises
--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
 })
