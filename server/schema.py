9967436d70ee7d3813876cf02d387cfdca1ea8df
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-04-12 15:41:11 +0300

    added schema validation

--- server/schema.py
+++ server/schema.py
@@ -1,10 +1,19 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validates_schema, ValidationError
 
 class ExerciseSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     category = fields.Str(required=True)
     equipment_needed = fields.Boolean(load_default=False)
+    
+    @validates_schema
+    def validate_schema(self, data, **kwargs):
+        valid_categories = ['Pilates', 'Yoga', 'Aerobics', 'Cardio', 'Bodybuilding']
+        if data['category'] not in valid_categories:
+            raise ValidationError('Category must be one of these: Pilates, Yoga, Aerobics, Cardio, Bodybuilding')
+        if not data['name'] or data['name'].strip() == '':
+            raise ValidationError('Name cannot be empty')
+        
     
     
 class WorkoutSchema(Schema):
@@ -13,6 +22,13 @@ class WorkoutSchema(Schema):
     duration_minutes = fields.Int(allow_none=True)
     notes = fields.Str(allow_none=True)
     
+    @validates_schema
+    def validate_schema(self, data, **kwargs):
+        if data.get('duration_minutes') is not None and data['duration_minutes'] < 0:
+            raise ValidationError('Duration minutes must be a positive numbrer')
+        if data.get('notes') is not None and len(data['notes']) > 300:
+            raise ValidationError('Notes must be less than 300 characters long')
+    
 class WorkoutExerciseSchema(Schema):
     id = fields.Int(dump_only=True)
     reps = fields.Int(allow_none=True)
@@ -20,5 +36,10 @@ class WorkoutExerciseSchema(Schema):
     duration_seconds = fields.Int(allow_none=True)
     workout_id = fields.Int(required=True)
     exercise_id = fields.Int(required=True)
+    
+    @validates_schema
+    def validate_schema(self, data, **kwargs):
+        if data.get('duration_seconds') is not None and data['duration_seconds'] < 0:
+            raise ValidationError('Duration seconds must be a positive number')
     
     
